7d0245a6fd60b66520bcdf8b645274163a49571e
Joseph Khan <joseph.khan@altayyargroup.com>
2018-11-19 16:06:53 +0400

    Redux devtools integration

--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,6 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    rootReducer, 
+    composeEnhancers(applyMiddleware(thunkMiddleware))
+);
 export default store;
